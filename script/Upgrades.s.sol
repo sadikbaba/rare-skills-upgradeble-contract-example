8d82f4c1bdd291363eed0981940c3e89b5157287
sadikbaba <4821saddikhady@gmail.com>
2026-02-04 13:53:10 +0100

    script file and depoloyed to sepolia

--- script/Upgrades.s.sol
+++ script/Upgrades.s.sol
@@ -12,9 +12,7 @@ contract UpgradesScript is Script {
     function setUp() public {}
 
     function run() public {
-     
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast();
 
         // Deploy `ContractA` as a transparent proxy using the Upgrades Plugin
         address transparentProxy =
